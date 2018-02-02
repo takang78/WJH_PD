99cc6e626fb8e3b4b4a8d026686bf3683ec9c9c9
230202 <230202@shakopeeschools.org>
2018-02-02 08:18:54 -0600

    Trying Again

--- team6.py
+++ team6.py
@@ -26,9 +26,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    if len(my_history)==0:
-        return 'c'
-    elif 'cbcb' or 'bbb' in their_history:
+    if 'cbcb' or 'bbb' in their_history:
         return 'b'
     else:
         return 'c'
