3b7f2cdea8904df14379cf6ecfe1ce473cbc2d43
ellie011 <227201@shakopeeschools.org>
2018-02-02 08:28:36 -0600

    More changes

--- team11.py
+++ team11.py
@@ -17,9 +17,10 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-    def rounds_played():
-        if len(my_history)>85:
-             return 'b'
+    def rounds_played_b(betrayed):
+        if 'b' in their_history:
+            betrayed = True
+            return betrayed
     
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
@@ -32,6 +33,8 @@ def move(my_history, their_history, my_score, their_score):
 
     if len(their_history)==0:
         return 'c'
+    if rounds_played_b(betrayed) and betrayed == True:
+        return 'b'
     elif 'ccc' in their_history:
         return 'b' 
     elif 'bbb' in their_history:
