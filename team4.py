c6d1508c759dc8f9b01d3fe0c7a67ea296b9c312
Saoumi <231303@shakopeeschools.org>
2018-02-02 07:51:12 -0600

    Merge branch 'Emitsun1-31'
    # Conflicts:
    #	tournament.txt

--- team4.py
+++ team4.py
@@ -38,37 +38,3 @@ def move(my_history, their_history, my_score, their_score):
     else:
         return 'c'
     
-
-    
-        
-
-    
-def test_move(my_history, their_history, my_score, their_score, result):
-    '''calls move(my_history, their_history, my_score, their_score)
-    from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
-    '''
-
-
-if __name__ == '__main__':
-     
-    # Test 1: Betray on first move.
-    if test_move(my_history='',
-              their_history='', 
-              my_score=0,
-              their_score=0,
-              result='b'):
-         print 'Test passed'
-     # Test 2: Continue betraying if they collude despite being betrayed.
-    test_move(my_history='bbb',
-              their_history='ccc', 
-              # Note the scores are for testing move().
-              # The history and scores don't need to match unless
-              # that is relevant to the test of move(). Here,
-              # the simulation (if working correctly) would have awarded 
-              # 300 to me and -750 to them. This test will pass if and only if
-              # move('bbb', 'ccc', 0, 0) returns 'b'.
-              my_score=0, 
-              their_score=0,
-              result='b')                 
-   
