19eaa8dbb6f35256a2fe6f76352ef70f676fd3fe
Freddy Melo <229017@shakopeeschools.org>
2018-02-05 08:21:02 -0600

    Strategy name and Description Team 12

--- team12.py
+++ team12.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'Panthers' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Collude, betray, and forgive'
+strategy_description = 'Colludes until a betray, but it will collude again if opponent colludes.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
