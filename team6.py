df01df88587d1e0e6407f9773e5d615d936f328d
230202 <230202@shakopeeschools.org>
2018-02-05 07:48:25 -0600

    Revert "Revert "Final""
    This reverts commit 18f396fe879f71225a54139e9e6bc7d6c781400e.

--- team6.py
+++ team6.py
@@ -26,9 +26,9 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    if 'cbcb' or 'bbb' in their_history:
-        return 'b'
-    else:
+    if 'ccc' in their_history or len(their_history) <= 100:
+        return 'c'
+    elif 'bbb' or 'cbcb' in their_history:
         return 'c'
     
     #Ideas below    
