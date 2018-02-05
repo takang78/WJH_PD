9287522cc359db965f859b8965dcca5d70c47b70
224773 <224773@c1mtk31qh3qd.isd720.com>
2018-02-05 07:51:35 -0600

    Got rid of procedure
    Got rid of our failed procedure

--- team3.py
+++ team3.py
@@ -10,7 +10,6 @@
 team_name = 'Danny DeVito' # Only 10 chars displayed.
 strategy_name = 'DeVito Devastator'
 strategy_description = 'How does this strategy decide?'
-fifty = False 
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
@@ -18,10 +17,7 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-    def number_rounds():
-        if len(my_history)==50:
-            fifty = False
-       
+   
     
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -31,13 +27,12 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    if len(my_history) <3 and fifty == False:
+    
+    if len(my_history) <3: 
         return 'c'
     elif their_history[-3]=='b':
         return 'b'
     elif their_history[-2]=='cb':
-        return 'b'
-    elif fifty == True:
         return 'b'
     else:
         return 'c'
