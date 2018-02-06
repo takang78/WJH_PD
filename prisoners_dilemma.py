b517f1d9795c35711c7dc13dec01ed7cc9908c58
Robert Mainhardt <rmainhar@shakopee.k12.mn.us>
2018-02-06 08:11:55 -0600

    removed team5

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -36,14 +36,14 @@ import os.path
 import example0, example1, example2, example3
 import example4, example5, example6, example8
 import team0, team1, team2, team3, team4
-import team5, team6, team7, team8, team9
-import team10, team11, team12, team13, team14
+import team6, team7, team8, team9
+import team10, team11, team12, team14
 betray = example1
 collude = example0
 
 modules = [example0, example1, example2, example3, example4, example5, example6, example8,
-team0, team1, team2, team3, team4, team5, team6, team7, team8, team9, team10, 
-team11, team12, team13, team14]
+team0, team1, team2, team3, team4, team6, team7, team8, team9, team10, 
+team11, team12, team14]
 for module in modules:
     reload(module)
     print ('reloaded',module)
@@ -378,5 +378,5 @@ def post_to_file(string, filename='tournament.txt', directory=''):
  
 ### Call main_play() if this file is executed
 if __name__ == '__main__':
-    scores, moves, reports = main_play(modules[0:24])   
+    scores, moves, reports = main_play(modules[0:23])   
     section0, section1, section2, section3 = reports
